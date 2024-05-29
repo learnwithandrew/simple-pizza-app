11e781bbc17a6ca5c571a1345756104f41472a06
andyjohn23 <andrewnick906@gmail.com>
2024-05-29 18:09:54 +0300

    create pizza data component

---
+++ src/Data.js
@@ -0,0 +1,20 @@
+export const pizzaData = [
+  {
+    id: 1,
+    toppings: "plain pizza",
+    size: "small",
+    vegetarian: true,
+  },
+  {
+    id: 2,
+    toppings: "sausage",
+    size: "large",
+    vegetarian: false,
+  },
+  {
+    id: 3,
+    toppings: "mushroom",
+    size: "small",
+    vegetarian: true,
+  },
+];
